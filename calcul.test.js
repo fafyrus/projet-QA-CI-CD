5137d9b9034dedd6db45cb51ff1c6ff92a182fc2
Farouk <f.bejaoui1@gmail.com>
2026-05-17 17:47:35 +0200

    fix: correction de la virgule

--- calcul.test.js
+++ calcul.test.js
@@ -1,1 +1,1 @@
-const additionner = require('./calcul'); test('ajoute 2 + 3 pour égaler 5' () => { expect(additionner(2, 3)).toBe(5);
+const additionner = require('./calcul'); test('ajoute 2 + 3 pour égaler 5', () => { expect(additionner(2,3)).toBe(5); });
